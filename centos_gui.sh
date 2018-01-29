65e6d34561db16b657d12b6d1317e5596141fb93
Jehwan Yun <a01054718605@gmail.com>
2018-01-29 12:00:10 +0900

    Update centos_gui.sh

--- centos_gui.sh
+++ centos_gui.sh
@@ -1,3 +1,7 @@
+# Tested on
+#   CentOS7 Everything version
+#   Virtualbox 5.2.6
+# Just run this script on root permission hoho
 dhclient
 yum groupinstall -y “X Window System” “GNOME Desktop”
 systemctl set-default graphical.target
