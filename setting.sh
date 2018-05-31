4d4e4c3802de75e667f586ec6e51260e2af828c9
Jehwan Yun <cluxebot@gmail.com>
2018-05-31 10:39:02 +0900

    Added Vim YouCompleteMe

--- setting.sh
+++ setting.sh
@@ -1,6 +1,31 @@
+"""
+Thanks to these guys
+	- http://www.alexeyshmalko.com/2014/youcompleteme-ultimate-autocomplete-plugin-for-vim/
+	- https://nolboo.kim/blog/2016/09/20/vim-plugin-manager-vundle/
+"""
+
+
+$pwd=$(pwd)
+
+# APT
+echo "[+] Updating and Upgrading APT List"
+apt-get update
+apt-get upgrade
+
 # Vundle
-echo "[+] Installing vim 'Vundle'"
+echo "[+] Installing vim.Vundle"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# YCM
+echo "[+] Installing vim.YouCompleteMe with semantic support for C-familiy languages"
+apt-get install python-minimal
+apt-get install build-essential cmake python-dev python3-dev
+cd ~/vim/bundle
+git clone https://github.com/Valloric/YouCompleteMe.git
+cd YouCompleteMe
+git submodule update --init --recursive
+./install.sh --clang-completer
+cd $pwd
 
 # .vimrc
 echo "[+] Getting .vimrc"
